c44719537d5095fa2c8964c1f3430824bd3ff09c
João Gabriel Leal de Andrade Barbosa <83425029+Mitsuketa2004@users.noreply.github.com>
2022-09-25 19:16:26 -0300

    v1.0
    Primeira versão do algoritmo funcionando com detalhes de descrição no readme e no código.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Busca-Binaria
  Estudos do algoritmo de busca binária, expandindo e explorando as possibilidades com esse algoritmo
+
+# Objetivos.
+1. (feito) Escrever o algoritmo de forma padrão e simples.
+2. Organização do algoritmo tornando sua principal funcionalidade uma função.
+3. fazer com que o algoritmo receba uma lista de possíveis objetos a serem encontrados no array.
+4. o algoritmo receber uma lista desordenada, ordena-la e logo em seguida cumprir o mesmo papel do objetivo 3.
+
+VERSÃO INGLÊS//ENGLHIS VERSION
+
+# BINARIE SEARCH
+ studies of the algorithm, binarie search, to expand the possibilites the learning points.
+
+# GOALS
+1. (done) write the code until it works in a simple way.
+2. making an organization in the to make the principal algorithm as a function.
+3. can receive a list of possibles number to be find in the array.
+4. the algorithm should receive a shuffled array, sorting in ascending order and making the same as third goal.
--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@ const int TAM =10;
 
 int main(){
     //Busca Binária.
-    int array[TAM] = {10,20,30,40,65,78,81,100,120,335}, oEscolhido;
-    int extremidadeMenor = *array - *array , extremidadeMaior = *array -1;
-    int meioAtual, i =0;
+    int array[TAM] = {10,20,30,40,65,78,81,100,120,335}, oEscolhido, extremidadeMenor = *array - *array , extremidadeMaior = *array -1, meioAtual;
     bool achou = true;
     //define os pontos de extremidades
-    //O pivot deve recorrer array usando a metade de sua totalidade como próximo passo
     std::cout <<"Escolha um Número dentro de :";
     for(int i = 0; i <= *array -1; i++){
         std::cout << array[i] << " ," ;
     }
     std::cin >> oEscolhido;
+    //imprime o array, e recebe um número. 
+
     while (achou){
         meioAtual = (extremidadeMenor + extremidadeMaior )/2;
+        //O pivot deve recorrer array usando a metade de sua totalidade como próximo passo.
         if (array[meioAtual] == oEscolhido || extremidadeMaior <= extremidadeMenor){
             achou = false;
             std::cout << array[meioAtual];
@@ -24,7 +24,9 @@ int main(){
         }else if (oEscolhido > array[meioAtual]){
             extremidadeMenor = meioAtual;
         }
+        //conjunto de verificações.
 
     }
+    //loop alterando as posições das extremidade(maior e menor) e do cursor(meioatual) até acharem o valor ou até as extremidades se cruzarem
     return 0;
     }
