7c9fa67b9a7f58da8136552f2900177e022185c2
João Gabriel Leal de Andrade Barbosa <83425029+Mitsuketa2004@users.noreply.github.com>
2022-09-25 19:52:05 -0300

    v1.1
    Feito Objetivo 2: "Organização do algoritmo tornando sua principal funcionalidade uma função"
    Bug arrumado de, numa extremidade maior, não se achava o alvo.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 # Objetivos.
 1. (feito) Escrever o algoritmo de forma padrão e simples.
-2. Organização do algoritmo tornando sua principal funcionalidade uma função.
+2. (feito) Organização do algoritmo tornando sua principal funcionalidade uma função.
 3. fazer com que o algoritmo receba uma lista de possíveis objetos a serem encontrados no array.
 4. o algoritmo receber uma lista desordenada, ordena-la e logo em seguida cumprir o mesmo papel do objetivo 3.
 
@@ -14,6 +14,6 @@ VERSÃO INGLÊS//ENGLHIS VERSION
 
 # GOALS
 1. (done) write the code until it works in a simple way.
-2. making an organization in the to make the principal algorithm as a function.
+2. (feito)making an organization in the to make the principal algorithm as a function.
 3. can receive a list of possibles number to be find in the array.
 4. the algorithm should receive a shuffled array, sorting in ascending order and making the same as third goal.
--- main.cpp
+++ main.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 const int TAM =10;
 
+int Buscar(int alvo, int array[]){
+    int extremidadeMenor = *array - *array , extremidadeMaior = *array, pivot;
+    bool achouAlvo = true;
+
+    while (achouAlvo){
+        pivot = (extremidadeMenor + extremidadeMaior )/2;
+        //O pivot deve recorrer array usando a metade de sua totalidade como próximo passo.
+        if (array[pivot] == alvo || extremidadeMaior <= extremidadeMenor){
+            achouAlvo = false;
+            std::cout << array[pivot];
+        }else if (alvo < array[pivot]){
+            extremidadeMaior = pivot;
+        }else if (alvo > array[pivot]){
+            extremidadeMenor = pivot;
+        }
+        //conjunto de verificações.
+
+    }
+      //loop alterando as posições das extremidade(maior e menor) e do cursor(pivot) até acharem o valor ou até as extremidades se cruzarem
+return array[pivot];
+}
+
 int main(){
     //Busca Binária.
-    int array[TAM] = {10,20,30,40,65,78,81,100,120,335}, oEscolhido, extremidadeMenor = *array - *array , extremidadeMaior = *array -1, meioAtual;
-    bool achou = true;
+    int array[TAM] = {10,20,30,40,65,78,81,100,120,335}, alvo, resultado;
     //define os pontos de extremidades
     std::cout <<"Escolha um Número dentro de :";
     for(int i = 0; i <= *array -1; i++){
         std::cout << array[i] << " ," ;
     }
-    std::cin >> oEscolhido;
-    //imprime o array, e recebe um número. 
-
-    while (achou){
-        meioAtual = (extremidadeMenor + extremidadeMaior )/2;
-        //O pivot deve recorrer array usando a metade de sua totalidade como próximo passo.
-        if (array[meioAtual] == oEscolhido || extremidadeMaior <= extremidadeMenor){
-            achou = false;
-            std::cout << array[meioAtual];
-        }else if (oEscolhido < array[meioAtual]){
-            extremidadeMaior = meioAtual;
-        }else if (oEscolhido > array[meioAtual]){
-            extremidadeMenor = meioAtual;
-        }
-        //conjunto de verificações.
-
-    }
-    //loop alterando as posições das extremidade(maior e menor) e do cursor(meioatual) até acharem o valor ou até as extremidades se cruzarem
+    std::cin >> alvo;
+    resultado = Buscar(alvo, array);
     return 0;
     }
